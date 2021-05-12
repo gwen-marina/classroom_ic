c0d0b7756a00c5c246eee1b59be2b55d7983d88d
Michael Abbott <michael@mjabbottdesign.com>
2021-05-12 14:01:11 -0700

    Add #yell_at_students method

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,14 +1,22 @@
 class Classroom
-  attr_reader :subject, :capacity, :students
+  attr_reader :subject, :capacity, :students, :capitalized_names
 
   def initialize(subject, capacity)
     @subject = subject
     @capacity = capacity
     @students = []
   end
-  
+
   def add_student(name)
     students << name
+  end
+
+  def yell_at_students
+    capitalized_names = []
+    students.each do |name|
+      capitalized_names << name.upcase
+    end
+    capitalized_names
   end
 
 end
